b4d8f0545877c084fd290b7ff15b9fcbf8372033
unknown <1186879640A@.area52.afnoapps.usaf.mil>
2011-09-16 15:12:49 -0500

    javadoc

--- src/main/java/org/jason/mapmaker/client/view/MapPanelUiHandlers.java
+++ src/main/java/org/jason/mapmaker/client/view/MapPanelUiHandlers.java
@@ -8,8 +8,17 @@ import com.gwtplatform.mvp.client.UiHandlers;
  *
  * @since 0.4.4
  * @author Jason Ferguson
+ * @see org.jason.mapmaker.client.view.MapmakerMapViewImpl
+ * @see org.jason.mapmaker.client.presenter.MapmakerMapViewPresenter
  */
 public interface MapPanelUiHandlers extends UiHandlers {
 
-    void doGetLocationDescriptions(JavaScriptObject map, double lng, double lat);
+    /**
+     * Display a marker with an infowindow on the given map at the given coordinates
+     *
+     * @param map   JavaScriptObject representing the Google Maps API v3 map object
+     * @param lng   double representing the longitude
+     * @param lat   double representing the latitude
+     */
+    void doGetLocationDescription(JavaScriptObject map, double lng, double lat);
 }
